16cc851f41651bc2c897db69cb3ff6a5cddfc67f
jcstone3 <jcstone3@ncsu.edu>
2015-09-15 17:29:13 -0400

    Initial PacketList done.

--- Project_1/src/PacketList.java
+++ Project_1/src/PacketList.java
@@ -4,36 +4,131 @@
  * @author Michael Mackrell
  * @author Jacob Stone
  * @author Curtis Moore
+ * 
+ * A sorted linked list of packets.
  */
-
 public class PacketList {
 	
-	public PacketNode head;
-	public PacketNode current;
-
-	public void add(){
-		// TODO Auto-generated method stub
+	/** The front of the list */
+	Node front;
+	
+	/** Simple constructor for PacketList */
+	public PacketList () {
+		front = null;
+	}
+	/** extended constructor for PacketList.
+	 * @param packetToStart the packet to start off the list
+	 */
+	public PacketList (Packet packetToStart) {
+		this.add(packetToStart);
 	}
 	
-	public PacketNode getHead() {
-		// TODO Auto-generated method stub
-		return head;
-	}
-
 	/**
-	public class PacketNode {
-
-		public String data;
-		public PacketNode next;
-
-		public PacketNode(String data) {
-			this(data, null);
+	 * Adds a packet to the list in ascending order
+	 * @param packetToAdd the packet to add to the list.
+	 */
+	public void add(Packet packetToAdd) {
+		if (front == null) {
+			front = new Node(packetToAdd, null);
+		} else {
+			this.addToRest(packetToAdd);
 		}
-
-		public PacketNode(String data, PacketNode next) {
-			this.data = data;
+	}	
+	
+	/**
+	 * Adds a packet to the rest of the list
+	 * PRE: the front of the list is not null
+	 * @param packetToAdd
+	 */
+	public void addToRest(Packet packetToAdd) {
+		Node current = front;
+		Node previous = front;
+		
+		if (packetToAdd.getPackNum() < front.packet.getPackNum()) {
+			front = new Node(packetToAdd, front);
+		} else {
+			
+			
+			while (current != null) {
+				int currentNum = current.packet.getPackNum();
+				int toAddNum = packetToAdd.getPackNum();
+				if ( toAddNum > currentNum) {
+					if (current.next == null) {
+						current.next = new Node(packetToAdd, null);
+						break;
+					} else {
+						previous = current;
+						current = current.next;
+					}
+				} else {
+					previous.next = new Node(packetToAdd, current);
+				}
+			}
+			
+		}
+		
+	}
+	
+	/**
+	 * Creates a string of the packet list
+	 * and returns it to the caller.
+	 * PRE: list is in ascending order
+	 * @param messageNum the message the list belongs to
+	 * @return listString the string of the packet list
+	 */
+	public String toString(int messageNum) {
+		Node current = front;
+		int nextNum = 1;
+		//boolean foundMatch = false;
+		String listString = "";
+		
+		while (current != null) {
+			if (nextNum == current.packet.getPackNum()) {
+				listString += current.packet.getMessage() + "\n";
+			} else {
+				while (nextNum != current.packet.getPackNum()) {
+					listString += "WARNING: packet " + nextNum + " of message ";
+					listString += messageNum + " is missing\n";
+					nextNum++;
+				}
+				listString += current.packet.getMessage() + "\n";
+			}
+			nextNum++;
+			current = current.next;
+		}
+		
+		return listString;
+	}
+	
+	/**
+	 * The node object used in a linked list.
+	 * @author Jacob Stone
+	 * @author Michael Mackrell
+	 * @author Jacob Stone
+	 * @author Curtis Moore
+	 */
+	private class Node {
+		
+		/**
+		 * The packet that the Node contains.
+		 */
+		public Packet packet;
+		
+		/**
+		 * The reference to the next Node in the list.
+		 */
+		public Node next;
+		
+		/**
+		 * Constructor for node objects.
+		 * @param device the device inside the node
+		 * @param next the reference to the next node
+		 */
+		public Node(Packet packet, Node next) {
+			this.packet = packet;
 			this.next = next;
 		}
-	} */
+	}
+	
 	
 }
