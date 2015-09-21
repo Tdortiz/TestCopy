5d5631ec293d6fa53e286d571937cd36b0791206
ThomasOrtiz <tdortiz@ncsu.edu>
2015-09-21 17:08:37 -0400

    Added test cases.

--- Project_1/test/MessageListTest.java
+++ Project_1/test/MessageListTest.java
@@ -5,36 +5,35 @@ import org.junit.Test;
 
 public class MessageListTest {
     MessageList messyList;
-    Message mes0;
     Message mes1;
+    Message mes2;
     Packet goPack;
     
     @Before
     public void setUp() throws Exception {
         messyList = new MessageList();
-        mes0 = new Message(0, 1, "testing");
-        mes1 = new Message(1, 1, "test2");
+        mes1 = new Message(1, 1, "testing");
+        mes2 = new Message(2, 1, "test2");
         goPack = new Packet(1, "123");
     }
 
     @Test
     public void testAdd() {
-        assertEquals(null, messyList.toString());
+        //assertEquals(null, messyList.toString());
         
-        mes0.add(goPack.getPackNum(), goPack.getMessage());
-        messyList.add(mes0);
         messyList.add(mes1);
-        
-        
-        assertEquals("1", messyList.head.data.toString());
+        String s = "--- Message 3\n";
+        	  s += "testing"; 
+        System.out.println(messyList.toString());
+        assertEquals(s, messyList.toString());
     }
     
     @Test
     public void testToString(){
         assertEquals(null, messyList.toString());
         
-        messyList.add(mes0);
         messyList.add(mes1);
+        messyList.add(mes2);
         
         //a set of sanity-check printlns to ensure nodes are added correctly.
         System.out.println(messyList.head.data.toString());
