63fbf14fefb0c3f8c6e1b1b923c15431cfe44cbc
ThomasOrtiz <tdortiz@ncsu.edu>
2015-09-20 22:59:36 -0400

    Added testToStringTwo

--- Project_1/test/PacketListTest.java
+++ Project_1/test/PacketListTest.java
@@ -25,6 +25,10 @@ public class PacketListTest {
 	
 	private Packet pack6;
 	
+	private Packet pack7;
+	
+	private Packet pack8;
+	
 	private String testString;
 	
 	private PacketList sixPack;
@@ -102,5 +106,40 @@ public class PacketListTest {
 		sixPack.add(pack4);
 		assertEquals(testString, sixPack.toString(3));
 	}
+	
+	/**
+	 * Test method for {@link PacketList#toString(int)}.
+	 */
+	@Test
+	public void testToStringTwo() {
+		pack1 = new Packet(1, "Good");
+		pack2 = new Packet(2, "Morning");
+		pack3 = new Packet(3, "Seven");
+		pack4 = new Packet(4, "yasss");
+		pack5 = new Packet(5, "Bad");
+		pack6 = new Packet(6, "Afternoon");
+		pack7 = new Packet(7, "Four");
+		pack8 = new Packet(8, "Nuuuu");
+		
+		
+		sixPack.add(pack6);
+		sixPack.add(pack7);
+		sixPack.add(pack8);
+		sixPack.add(pack1);
+		sixPack.add(pack3);
+		sixPack.add(pack2);
+		sixPack.add(pack5); // 1 2 3 5 6 7 8
+		
+		testString = "Good\n";
+		testString += "Morning\n";
+		testString += "Seven\n";
+		testString += "WARNING: packet 4 of message 3 is missing\n";
+		testString += "Bad\n";
+		testString += "Afternoon\n";
+		testString += "Four\n";
+		testString += "Nuuuu\n";
+		assertEquals(testString, sixPack.toString(3));
+	}
+	
 
 }
