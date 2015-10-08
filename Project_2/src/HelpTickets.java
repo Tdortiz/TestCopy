6adaf5ea49ed45aa413ec2b8aed8865bb4cecfb5
ThomasOrtiz <tdortiz@ncsu.edu>
2015-10-08 12:10:01 -0400

    Main file.

---
+++ Project_2/src/HelpTickets.java
@@ -0,0 +1,12 @@
+/**
+ * main program will be in HelpTickets.java, but you are strongly encouraged to
+ * encapsulate and separately test whatever you can in other classes.
+ * @author Thomas Ortiz
+ *
+ */
+public class HelpTickets {
+	
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+	}
+}
