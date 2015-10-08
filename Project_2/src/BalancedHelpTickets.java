696878b1af6284e7b28ef3fa2b85c729c3ae1e76
ThomasOrtiz <tdortiz@ncsu.edu>
2015-10-08 12:09:38 -0400

    Extra Credit File

---
+++ Project_2/src/BalancedHelpTickets.java
@@ -0,0 +1,16 @@
+/**
+ * should look exactly like HelpTickets.java, except that the part that instantiates
+ * a binary search tree will create a balanced one. Only one line should be different when we do a diff
+ * between HelpTickets.java and BalancedHelpTickets.java – the remaining differences should be 
+ * in the form of additional source files to implement the balanced tree.
+ * 
+ * @author Thomas Ortiz
+ */
+public class BalancedHelpTickets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
