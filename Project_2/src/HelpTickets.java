799e65099ddfb329185d8942ff61387527d2ea23
ThomasOrtiz <tdortiz@ncsu.edu>
2015-10-18 23:14:08 -0400

    Started parsing.

--- Project_2/src/HelpTickets.java
+++ Project_2/src/HelpTickets.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * main program will be in HelpTickets.java, but you are strongly encouraged to
  * encapsulate and separately test whatever you can in other classes.
@@ -14,6 +17,36 @@ public class HelpTickets {
 		/**  The main program should only parse commands, call methods corresponding to each command
 		 *  — methods of the wrapper class — and catch exceptions, passing these to the Warning class.
 		 */
+		
+		Scanner input = new Scanner(System.in);
+		Scanner line = null;
+		String command = null;
+		int num = 0;
+		
+		while( input.hasNextLine() ){
+			line = new Scanner(input.nextLine());
+			
+			// TODO Handle warnings 
+			command = line.next();
+			num = line.nextInt();
+
+			if(command.equals("+")){
+				// TODO Call wrapper methods for add
+				
+			} else if(command.equals("-")){
+				// TODO Call wrapper methods for remove
+				
+			} else if(command.equals("*")){
+				// TODO Call wrapper methods for remove highest priority
+				
+			} else if(command.equals("?")){
+				// TODO Call wrapper methods for current position in queue of the ticket with given id
+				
+			}
+		}
+		
+		line.close();
+		input.close();
 
 	}
 }
