9938415cb24e522bcb95f8b50d7baaeb3e51665d
ThomasOrtiz <tdortiz@ncsu.edu>
2015-10-18 23:29:46 -0400

    Think I finished the Warning class but we'll see in a week.

--- Project_2/src/Warning.java
+++ Project_2/src/Warning.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Class that handles all warnings. 
  * @author Thomas Ortiz
  * @author Michael Mackrell
  * @author Jacob Stone
@@ -7,4 +7,55 @@
  */
 public class Warning {
 
+	/**
+	 * Simple constructor for warning.
+	 */
+	public Warning(){}
+	
+	/**
+	 * Prints a warning message if the priority entered is already in the queue.
+	 * @param p priority entered.
+	 */
+	public void priorityAlreadyInQueue(int p){
+		System.out.println("Warning: a ticket with priority " + p + " is already in the queue");
+	}
+	
+	/**
+	 * Prints a warning message if the id is not in the queue.
+	 * @param i id entered.
+	 */
+	public void IdNotInQueue(int i){
+		System.out.println("Warning: there is no ticket with id = " + i + " in the queue"); 
+	}
+	
+	/**
+	 * Prints a warning message if the user attempts to remove from an empty queue.
+	 */
+	public void queueEmpty(){
+		System.out.println("Warning: removal attempted when queue is empty");
+	}
+	
+	/**
+	 * Prints a warning message if the user input the incorrect command.
+	 * @param command entered by user.
+	 */
+	public void invalidCommand(String command){
+		System.out.println("Warning: invalid command " +  command); 
+	}
+	
+	/**
+	 * Prints a warning message if the id entered is not an integer.
+	 * @param id entered by user.
+	 */
+	public void idNotInteger(int id){
+		System.out.println("Warning: id " + id + " is not an integer");
+	}
+	/**
+	 * Prints a warning message if the priority is not an integer.
+	 * @param p priority entered by user.
+	 */
+	public void pNotInteger(int p){
+		System.out.println("Warning: priority " + p + " is not an integer");
+	}
+	
 }
