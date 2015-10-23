834e19e913b1afc8462a999a5f2bf86b098a2d8b
mmackre <mmackre@ncsu.edu>
2015-10-23 19:08:21 -0400

    Ticket done and javadoced unless we need a to String

--- Project_2/src/Ticket.java
+++ Project_2/src/Ticket.java
@@ -7,10 +7,25 @@
  */
 public class Ticket {
 
+	/**
+	 * priority of the ticket
+	 */
 	private int priority;
+	/**
+	 * id of the ticket
+	 */
 	private int id;
+	/**
+	 * count of decendants of the ticket
+	 */
 	private int descendants; // count 
 	
+	/**
+	 * constructor for the ticket
+	 * @param priority the priority of the ticket
+	 * @param id the id of the ticket 
+	 * @param decendants the number of decendants of the ticket
+	 */
 	public Ticket(int priority, int id, int decendants){
 		this.priority=priority;
 		this.id = id;
@@ -18,10 +33,18 @@ public class Ticket {
 		
 	}
 	
+	/**
+	 * returns the ID of the ticket
+	 * @return id the id of the ticket
+	 */
 	public int getId(){
 		return id;
 	}
 	
+	/**
+	 * returns the priority of the ticket
+	 * @return priority the priority of the ticket
+	 */
 	public int getPriority(){
 		return priority;
 	}
