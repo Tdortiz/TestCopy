f24cd2e4d82a979c6f3d0eb334e4ebbcc3438dab
Thomas Ortz <tdortiz@ncsu.edu>
2015-10-29 15:20:26 -0400

    Fixed minor formatting.

--- Project_2/src/CommandHandler.java
+++ Project_2/src/CommandHandler.java
@@ -1,6 +1,8 @@
 import java.util.*;
+
 /**
  * Wrapper class that handles the command input.
+ * 
  * @author Thomas Ortiz
  * @author Michael Mackrell
  * @author Jacob Stone
@@ -9,48 +11,47 @@ import java.util.*;
 public class CommandHandler {
 
 	private TreeMap<Integer, Integer> map; // TreeMap
-	private Tree tree; // Tree 
+	private Tree tree; // Tree
 	private int idCounter;
-	
-	public CommandHandler(){
+
+	public CommandHandler() {
 		this.tree = new Tree();
 		map = new TreeMap<Integer, Integer>();
 		idCounter = 1;
-		
 	}
-	
-	public void insert(int priority){
+
+	public void insert(int priority) {
 		System.out.println("+ " + priority);
-		
-		Ticket t = new Ticket(priority, idCounter);//prioirty id descendants
+
+		Ticket t = new Ticket(priority, idCounter);// prioirty id descendants
 		map.put(idCounter, priority); // id is key, priority is value
-		tree.insert(t); 
+		tree.insert(t);
 		System.out.println("    id = " + idCounter);
-		
+
 		idCounter++;
 	}
-	
-	public void remove(int id){
+
+	public void remove(int id) {
 		System.out.println("- " + id);
-		
+
 		int priority = map.get(id);
 		map.remove(priority);
 		tree.remove(priority);
-		System.out.println(priority + ", pos = "); // + position in queue based off of descendants
+		System.out.println("    " + priority + ", pos = "); // + position in queue based off of descendants
 		// TODO FIX THIS POSTION THING
 	}
-	
-	public void removeHighest(){
+
+	public void removeHighest() {
 		System.out.println("*");
-		
+
 		int highest = map.lastKey();
-		tree.remove(highest);;
+		tree.remove(highest);
 		map.remove(highest);
 	}
-	
-	public void query(int id){
+
+	public void query(int id) {
 		System.out.println("? " + id);
-		
+
 		int priority = map.get(id);
 		tree.query(priority);
 	}
