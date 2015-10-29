6dbb423a7513f1f38be1a49fed1046cbbdbb32e1
Thomas Ortz <tdortiz@ncsu.edu>
2015-10-29 15:19:44 -0400

    Fixed query's paramaters

--- Project_2/src/Tree.java
+++ Project_2/src/Tree.java
@@ -10,25 +10,26 @@ import java.io.PrintStream;
 public class Tree {
 
 	private Node overallRoot; // null for an empty tree
-	
+
 	/**
 	 * Base constructor for Tree.
 	 */
-	public Tree(){
+	public Tree() {
 		overallRoot = null;
 	}
-	
+
 	/**
 	 * Inserts a call with a given priority
 	 */
-	public void insert(Ticket ticketToAdd){
-		// Any attempt to insert a ticket with the same priority as one already in the queue should generate a warning.
-		// Add based off of a ticket's priority 
+	public void insert(Ticket ticketToAdd) {
+		// Any attempt to insert a ticket with the same priority as one already
+		// in the queue should generate a warning.
+		// Add based off of a ticket's priority
 		overallRoot = add(overallRoot, ticketToAdd);
 	}
-	
+
 	public Node add(Node root, Ticket ticketToAdd) {
-		if (root == null){
+		if (root == null) {
 			root = new Node(ticketToAdd);
 		} else if (ticketToAdd.getPriority() < root.data.getPriority()) {
 			root.left = add(root.left, ticketToAdd);
@@ -37,141 +38,145 @@ public class Tree {
 		}
 		return root;
 	}
-	
-	public void remove(int priortyToRemove){
+
+	public void remove(int priortyToRemove) {
 		remove(priortyToRemove, overallRoot);
 	}
-	
+
 	/**
-	 *  Remove a call (using help ticket id)
+	 * Remove a call (using help ticket id)
 	 */
-	public void remove(int priortyToRemove, Node root){
-		// A similar warning should be generated if there is an attempt to remove a ticket that is not in the queue
-		if (priortyToRemove == root.data.getPriority()){
+	public void remove(int priortyToRemove, Node root) {
+		// A similar warning should be generated if there is an attempt to
+		// remove a ticket that is not in the queue
+		if (priortyToRemove == root.data.getPriority()) {
 			Node temp = root.left;
-			
+
 		}
 	}
-	
+
 	/**
 	 * Identify and remove highest priority call.
 	 */
-	public void removeHighest(){
+	public void removeHighest() {
 		// possibly return the highest priority node?
-		// Finally, there should be a warning if the queue is empty when a * command is issued. These warnings should read
+		// Finally, there should be a warning if the queue is empty when a *
+		// command is issued. These warnings should read
 	}
-	
-	/** 
-	 * Query about position in the queue (using help ticket id) 
+
+	/**
+	 * Query about position in the queue (using help ticket id)
+	 * 
 	 * @param priority
 	 * @return
 	 */
-	public int query(int priority){
+	public int query(int priority) {
 		return 0;
 	}
-	
-	/** 
+
+	/**
 	 * Checks if the tree contains a node.
+	 * 
 	 * @return true if tree does contain, false if not.
 	 */
-	public boolean contains(){
+	public boolean contains() {
 		return false;
 	}
-		
+
 	/**
 	 * Returns the current root of the tree.
+	 * 
 	 * @return root of tree.
 	 */
 	public Node getRoot() {
-		return root;
+		return this.overallRoot;
 	}
-	
-	/** 
+
+	/**
 	 * Sets method for root field.
-	 * @param root to set this.root to.
+	 * 
+	 * @param root
+	 *            to set this.root to.
 	 */
 	public void setRoot(Node root) {
-		this.root = root;
+		this.overallRoot = root;
 	}
+
+/** ------------------------------------THIS IS CODE GIVEN TO US ------------------------------------------*/
 	
-	
-	
-	
-	
-/** ------------------------------------THIS IS CODE GIVEN TO US ------------------------------------------*/	
 	/**
 	 * This is just a code fragment to illustrate a way to print a binary search
-	 * tree. 
-	 * prints the subtree rooted at v using the given indenting printer
+	 * tree. prints the subtree rooted at v using the given indenting printer
 	 */
-	  protected void recursiveInOrderPrint( BTPosition v, IndentPrinter printer )
-	  {
-	    checkPosition(v);
-	    if ( hasRight(v) )
-	      {
-	        printer.increaseIndent();
-	        BTPosition rv = right(v);
-	        recursiveInOrderPrint( rv, printer );
-	        printer.decreaseIndent();
-	      }
-	    printer.println( "-- value  = " + v.element() );
-	    if ( hasLeft(v) )
-	      {
-	        printer.increaseIndent();
-	        BTPosition lv = left(v);
-	        recursiveInOrderPrint( lv, printer );
-	        printer.decreaseIndent();
-	      }
-	  }
+	protected void recursiveInOrderPrint(BTPosition v, IndentPrinter printer) {
+		checkPosition(v);
+		if (hasRight(v)) {
+			printer.increaseIndent();
+			BTPosition rv = right(v);
+			recursiveInOrderPrint(rv, printer);
+			printer.decreaseIndent();
+		}
+		printer.println("-- value  = " + v.element());
+		if (hasLeft(v)) {
+			printer.increaseIndent();
+			BTPosition lv = left(v);
+			recursiveInOrderPrint(lv, printer);
+			printer.decreaseIndent();
+		}
+	}
 
-	  /**
-	   * prints the tree using inorder indenting subtree below each node;
-	   * uses backward inorder so that turning the printout sideways has the
-	   * correct left/right orientation
-	   * @param ps where to print
-	   * @param indentString what to print for each indentation level
-	   */
-	  public void inOrderPrint( PrintStream ps, String indentString )
-	  {
-	    if( root == null )
-	      {
-	        ps.println("EMPTY TREE");
-	        return;
-	      } 
-	    IndentPrinter printer = new IndentPrinter( ps, indentString );
-	    recursiveInOrderPrint( root, printer ); 
-	  } 
+	/**
+	 * prints the tree using inorder indenting subtree below each node; uses
+	 * backward inorder so that turning the printout sideways has the correct
+	 * left/right orientation
+	 * 
+	 * @param ps
+	 *            where to print
+	 * @param indentString
+	 *            what to print for each indentation level
+	 */
+	public void inOrderPrint(PrintStream ps, String indentString) {
+		if (root == null) {
+			ps.println("EMPTY TREE");
+			return;
+		}
+		IndentPrinter printer = new IndentPrinter(ps, indentString);
+		recursiveInOrderPrint(root, printer);
+	}
 
-	  /**
-	   * Node Class for Tree.
-	   * @author Thomas Ortiz
-	   * @author Michael Mackrell
-	   * @author Jacob Stone
-	   * @author Curtis Moore
-	   */
-	  public class Node {
-		  public Ticket data; // data stored at this node
-		  public Node left; // reference to left subtree
-		  public Node right; // reference to right subtree
-		  
-		  /**
-		   *  Constructs a leaf node with the given data.
-		   * @param data ticket 
-		   */
-		  public Node(Ticket data) {
-			  this(data, null, null);
-		  }
-			
-		  /**
-		   * Constructs a branch node with the given data and links.
-		   * @param data ticket
-		   * @param left node 
-		   * @param right node
-		   */
-		  public Node(Ticket data, Node left, Node right) {
-				this.data = data;
-				this.left = left;
-				this.right = right;
-		 }
-	  }
+	/**
+	 * Node Class for Tree.
+	 * 
+	 * @author Thomas Ortiz
+	 * @author Michael Mackrell
+	 * @author Jacob Stone
+	 * @author Curtis Moore
+	 */
+	public class Node {
+		public Ticket data; // data stored at this node
+		public Node left; // reference to left subtree
+		public Node right; // reference to right subtree
+
+		/**
+		 * Constructs a leaf node with the given data.
+		 * 
+		 * @param data ticket
+		 */
+		public Node(Ticket data) {
+			this(data, null, null);
+		}
+
+		/**
+		 * Constructs a branch node with the given data and links.
+		 * 
+		 * @param data ticket
+		 * @param left node
+		 * @param right node
+		 */
+		public Node(Ticket data, Node left, Node right) {
+			this.data = data;
+			this.left = left;
+			this.right = right;
+		}
+	}
 }
