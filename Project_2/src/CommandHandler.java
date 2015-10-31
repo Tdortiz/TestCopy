5d0a59c9433788c0ab21e9b9cc2b02ab70658c3b
Thomas Ortiz <tdortiz@ncsu.edu>
2015-10-31 19:20:14 -0400

    asd

--- Project_2/src/CommandHandler.java
+++ Project_2/src/CommandHandler.java
@@ -12,7 +12,7 @@ public class CommandHandler {
 
 	private TreeMap<Integer, Integer> map; // TreeMap
 	private Tree tree; // Tree
-	private int idCounter;
+	private int idCounter; // id incrementer 
 
 	public CommandHandler() {
 		this.tree = new Tree();
@@ -21,39 +21,56 @@ public class CommandHandler {
 	}
 
 	public void insert(int priority) {
-		System.out.println("+ " + priority);
-
-		Ticket t = new Ticket(priority, idCounter);// prioirty id descendants
-		map.put(idCounter, priority); // id is key, priority is value
-		tree.insert(t);
-		System.out.println("    id = " + idCounter);
-
-		idCounter++;
+		if(map.containsValue(priority)){
+			System.out.println("+ " + priority);
+			Ticket t = new Ticket(priority, idCounter);// prioirty id descendants
+			map.put(idCounter, priority); // id is key, priority is value
+			tree.insert(t);
+			System.out.println("    id = " + idCounter);
+	
+			idCounter++;
+		} else {  
+			// TODO say Warning: a ticket with priority p is already in the queue
+		}
 	}
 
 	public void remove(int id) {
-		System.out.println("- " + id);
-
-		int priority = map.get(id);
-		map.remove(priority);
-		
-		tree.remove(priority);
-		System.out.println("    " + priority + ", pos = "); // + position in queue based off of descendants
-		// TODO FIX THIS POSTION THING
+		// make isEmpty
+		if( map.containsKey(id) ){
+			System.out.println("- " + id);
+	
+			int priority = map.get(id);
+			map.remove(priority);
+			
+			tree.remove(priority);
+			System.out.println("    " + priority + ", pos = "); // + position in queue based off of descendants
+			// TODO FIX THIS POSTION THING
+		} else {
+			// Warning: there is no ticket with id = i in the queue 
+			// Warning: removal attempted when queue is empty
+		}
 	}
 
 	public void removeHighest() {
+		// if !isempty() {
 		System.out.println("*");
 
 		int highest = map.lastKey();
 		tree.remove(highest);
 		map.remove(highest);
+		// } else { 
+		// Warning: there is no ticket with id = i in the queue 
+		// Warning: removal attempted when queue is empty
 	}
 
 	public void query(int id) {
-		System.out.println("? " + id);
-
-		int priority = map.get(id);
-		tree.query(priority);
+		if( map.containsKey(id) ){ 
+			System.out.println("? " + id);
+	
+			int priority = map.get(id);
+			tree.query(priority);
+		} else {
+			// Warning: there is no ticket with id = i in the queue
+		}
 	}
 }
