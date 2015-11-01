0c417f661932a78477f9da4d0a89a170571cffe4
Thomas Ortiz <tdortiz@ncsu.edu>
2015-11-01 15:12:49 -0500

    Extra Credit

--- Project_2/src/BalancedHelpTickets.java
+++ Project_2/src/BalancedHelpTickets.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
+
 /**
- * should look exactly like HelpTickets.java, except that the part that instantiates
- * a binary search tree will create a balanced one. Only one line should be different when we do a diff
- * between HelpTickets.java and BalancedHelpTickets.java – the remaining differences should be 
- * in the form of additional source files to implement the balanced tree.
+ * The main program, reads in input from standard input, or in our case, a file, and 
+ * parses through each command passing them to the CommandHandler class. HelpTickets 
+ * also handles all exceptions.
  * 
  * @author Thomas Ortiz
  * @author Michael Mackrell
@@ -11,9 +12,68 @@
  */
 public class BalancedHelpTickets {
 
+	/**
+	 * The main program parses commands from a file, and then calls methods corresponding
+	 * to each command — methods of the wrapper class — and also catches exceptions.
+	 * @param args command line arguments
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		input(input);
 	}
+	
+    /**
+     * Handles all input from the user/file.
+     * @param input the input scanner passed from main
+     */
+	public static void input(Scanner input) {
+		Tree tree = new Tree(); // Simple binary search tree
+		CommandHandler handler = new CommandHandler(tree); // Handles all commands
+		Scanner lineParser = null; // Parses through a line
+		String command = null; // Command from user.
 
+		while (input.hasNextLine()) { // Runs until there is no more input
+			lineParser = new Scanner(input.nextLine());
+			command = lineParser.next(); // gets the commands +, -, *, ?
+
+			try {
+				if (command.equals("+")) { // ADD
+					String str = lineParser.next().trim();
+					try {
+						handler.insert( Integer.parseInt(str) );
+					} catch (NumberFormatException NFE) {
+						lineParser.close();
+						throw new Warning("Warning: priority " + str + " is not an integer");
+					}
+				} else if (command.equals("-")) { // REMOVE
+					String str = lineParser.next().trim();
+					try {
+						handler.remove( Integer.parseInt(str) );
+					} catch (NumberFormatException NFE) {
+						lineParser.close();
+						throw new Warning("Warning: id " + str + " is not an integer");
+					}
+				} else if (command.equals("*")) { // REMOVE HIGHEST
+					handler.removeHighest();
+
+				} else if (command.equals("?")) { // QUERY 
+					String str = lineParser.next().trim();
+					try {
+						handler.query( Integer.parseInt(str) );
+					} catch (NumberFormatException NFE) {
+						lineParser.close();
+						throw new Warning("Warning: id " + str + " is not an integer");
+					}
+				} else {
+					lineParser.close();
+					throw new Warning("Invalid command: " + command);
+				}
+			} catch (Warning invalidCommand) {
+				System.out.println(invalidCommand.getMessage());
+			}
+		}
+
+		lineParser.close();
+		input.close();
+	}
 }
