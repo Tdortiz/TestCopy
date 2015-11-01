fe25d3995d7bfb87aca013768c1c1853183c70d6
jcstone3 <jcstone3@ncsu.edu>
2015-10-31 20:34:32 -0400

    Jacob's

--- Project_2/src/Tree.java
+++ Project_2/src/Tree.java
@@ -133,9 +133,11 @@ public class Tree {
 	}
 	
 	private int query( Node root, int p ) {
+		if (root == null)
+			return 0;
 		if ( root.data.getPriority() == p)
 			return root.right.data.getDescendants();
-		if ( root.data.getPriority() > p)
+		if ( root.data.getPriority() > p )
 		    return query(root.left, p) + 1 + root.right.data.getDescendants();
 		else // ( root.data.getPriority() < p)
 		    return query(root.right, p);
@@ -211,47 +213,6 @@ public class Tree {
 		}
 	}
 
-/** ------------------------------------THIS IS CODE GIVEN TO US ------------------------------------------*/
-	
-	/**
-	 * This is just a code fragment to illustrate a way to print a binary search
-	 * tree. prints the subtree rooted at v using the given indenting printer
-	 */
-	/**protected void recursiveInOrderPrint(BTPosition v, IndentPrinter printer) {
-		checkPosition(v);
-		if (hasRight(v)) {
-			printer.increaseIndent();
-			BTPosition rv = right(v);
-			recursiveInOrderPrint(rv, printer);
-			printer.decreaseIndent();
-		}
-		printer.println("-- value  = " + v.element());
-		if (hasLeft(v)) {
-			printer.increaseIndent();
-			BTPosition lv = left(v);
-			recursiveInOrderPrint(lv, printer);
-			printer.decreaseIndent();
-		}
-	} */
-
-	/**
-	 * prints the tree using inorder indenting subtree below each node; uses
-	 * backward inorder so that turning the printout sideways has the correct
-	 * left/right orientation
-	 * 
-	 * @param ps
-	 *            where to print
-	 * @param indentString
-	 *            what to print for each indentation level
-	 */
-	/**public void inOrderPrint(PrintStream ps, String indentString) {
-		if (root == null) {
-			ps.println("EMPTY TREE");
-			return;
-		}
-		IndentPrinter printer = new IndentPrinter(ps, indentString);
-		recursiveInOrderPrint(root, printer);
-	} */
 
 	/**
 	 * Node Class for Tree.
