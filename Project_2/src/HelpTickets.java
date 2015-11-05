cd3e8a90fc29129cc7f8b50a412f03f43b383a6c
Curtis <Curtis@Curtis-PC>
2015-11-04 22:33:16 -0500

    Now with cleaner helptickets

--- Project_2/src/HelpTickets.java
+++ Project_2/src/HelpTickets.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 /**
- * The main program, reads in input from standard input, or in our case, a file, and 
- * parses through each command passing them to the CommandHandler class. HelpTickets 
- * also handles all exceptions.
+ * The main program, reads in input from standard input, or in our case, a file,
+ * and parses through each command passing them to the CommandHandler class.
+ * HelpTickets also handles all exceptions.
  * 
  * @author Thomas Ortiz
  * @author Michael Mackrell
@@ -13,35 +13,41 @@ import java.util.Scanner;
 public class HelpTickets {
 
 	/**
-	 * The main program parses commands from a file, and then calls methods corresponding
-	 * to each command — methods of the wrapper class — and also catches exceptions.
-	 * @param args command line arguments
+	 * The main program parses commands from a file, and then calls methods
+	 * corresponding to each command — methods of the wrapper class — and also
+	 * catches exceptions.
+	 * 
+	 * @param args
+	 *            command line arguments
 	 */
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		input(input);
 	}
-	
-    /**
-     * Handles all input from the user/file.
-     * @param input the input scanner passed from main
-     */
+
+	/**
+	 * Handles all input from the user/file.
+	 * 
+	 * @param input
+	 *            the input scanner passed from main
+	 */
 	public static void input(Scanner input) {
 		Tree tree = new Tree(); // Simple binary search tree
-		CommandHandler handler = new CommandHandler(tree); // Handles all commands
+		CommandHandler handler = new CommandHandler(tree); // Handles all
+															// commands
 		Scanner lineParser = null; // Parses through a line
 		String command = null; // Command from user.
 
 		while (input.hasNextLine()) { // Runs until there is no more input
 			lineParser = new Scanner(input.nextLine());
-			while(lineParser.hasNext() ){
+			while (lineParser.hasNext()) {
 				command = lineParser.next(); // gets the commands +, -, *, ?
-	
+
 				try {
 					if (command.equals("+")) { // ADD
 						String str = lineParser.next().trim();
 						try {
-							handler.insert( Integer.parseInt(str) );
+							handler.insert(Integer.parseInt(str));
 						} catch (NumberFormatException NFE) {
 							lineParser.close();
 							throw new Warning("Warning: priority " + str + " is not an integer");
@@ -49,18 +55,18 @@ public class HelpTickets {
 					} else if (command.equals("-")) { // REMOVE
 						String str = lineParser.next().trim();
 						try {
-							handler.remove( Integer.parseInt(str) );
+							handler.remove(Integer.parseInt(str));
 						} catch (NumberFormatException NFE) {
 							lineParser.close();
 							throw new Warning("Warning: id " + str + " is not an integer");
 						}
 					} else if (command.equals("*")) { // REMOVE HIGHEST
 						handler.removeHighest();
-	
-					} else if (command.equals("?")) { // QUERY 
+
+					} else if (command.equals("?")) { // QUERY
 						String str = lineParser.next().trim();
 						try {
-							handler.query( Integer.parseInt(str) );
+							handler.query(Integer.parseInt(str));
 						} catch (NumberFormatException NFE) {
 							lineParser.close();
 							throw new Warning("Warning: id " + str + " is not an integer");
@@ -74,7 +80,6 @@ public class HelpTickets {
 				}
 			}
 		}
-		tree.printInorder();
 
 		lineParser.close();
 		input.close();
