2c404fdfb0cb5b971c7932fada31f745aef89973
Thomas Ortiz <tdortiz@ncsu.edu>
2015-11-04 23:20:30 -0500

    Reimplemented with his code.

--- Project_2/src/HelpTickets.java
+++ Project_2/src/HelpTickets.java
@@ -21,8 +21,21 @@ public class HelpTickets {
 	 *            command line arguments
 	 */
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		input(input);
+		//Scanner input = new Scanner(System.in);
+		//input(input);
+		
+		Tree tree = new Tree(); // Simple binary search tree
+		CommandHandler handler = new CommandHandler(tree);
+	    Scanner scanInput = new Scanner( System.in );
+	    
+	    boolean keepGoing = true;
+	    while ( keepGoing ){
+	        try {
+	          keepGoing = nextCommand(handler, scanInput);
+	        } catch ( Warning w ) {
+	          System.out.println(w);
+	        }
+	    }
 	}
 
 	/**
@@ -84,4 +97,86 @@ public class HelpTickets {
 		lineParser.close();
 		input.close();
 	}
+
+	public static boolean nextCommand(CommandHandler handler, Scanner input){
+		if ( ! input.hasNextLine() ) return false;
+		
+	    String line = input.nextLine();
+	    Scanner lineScanner = new Scanner( line );
+	    
+	    // check for blank line
+	    if ( ! lineScanner.hasNext() ) throw new Warning("blank line");
+
+	    // command is the first token of the line; look at it and decide what to
+	    // do next
+	    String command = lineScanner.next();
+	    if ( command.equals( "+" ) ) handleAdd(handler, lineScanner);
+	    else if ( command.equals( "-" ) ) handleRemove(handler,lineScanner);
+	    else if ( command.equals( "?" ) ) handleQuery(handler,lineScanner);
+	    else if ( command.equals( "*" ) ) handleRemoveHighest(handler);
+	    else throw new Warning("invalid command " + command);
+	    
+	    if ( lineScanner.hasNext() )
+	      throw new Warning("Extra stuff on command line, starting with "
+	                        + lineScanner.next());
+	    return true;
+	}
+	
+	 private static void handleAdd(CommandHandler handler, Scanner lineScanner) throws Warning
+	  {
+	    if ( ! lineScanner.hasNext() ) throw new Warning( "missing priority for +" );
+	    
+	    int priority;
+	    String pString = "";
+	    
+	    try {
+	      pString = lineScanner.next();
+	      priority = Integer.parseInt( pString );
+	    }
+	    catch ( NumberFormatException nfe ) {
+	      throw new Warning("priority " + pString + " is not an integer");
+	    }
+	    
+	    handler.insert(priority);
+	  }
+
+	  private static void handleRemove(CommandHandler handler, Scanner lineScanner) throws Warning
+	  {
+	    if ( ! lineScanner.hasNext() ) throw new Warning( "missing id for -" );
+	    
+	    int id;
+	    String idString = "";
+	    try {
+	      idString = lineScanner.next();
+	      id = Integer.parseInt( idString );
+	    }
+	    catch ( NumberFormatException nfe ) {
+	      throw new Warning("id " + idString + " is not an integer");
+	    }
+	    
+	    handler.remove( id );
+	  }
+
+	  private static void handleQuery(CommandHandler handler, Scanner lineScanner) throws Warning
+	  {
+	    if ( ! lineScanner.hasNext() ) throw new Warning( "missing id for ?" );
+	    
+	    int id;
+	    String idString = "";
+	    
+	    try {
+	      idString = lineScanner.next();
+	      id = Integer.parseInt( idString );
+	    }
+	    catch ( NumberFormatException nfe ) {
+	      throw new Warning("id " + idString + " is not an integer");
+	    }
+	    
+	    handler.query(id);
+	  }
+
+	  private static void handleRemoveHighest(CommandHandler handler) throws Warning
+	  {
+	    handler.removeHighest();
+	  }
 }
