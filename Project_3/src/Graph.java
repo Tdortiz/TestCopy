66de1d6e7d00ecbfa5035b67d3645c5c26f3e6d6
Curtis <Curtis@Curtis-PC>
2015-11-15 17:06:35 -0500

    There. Code.

--- Project_3/src/Graph.java
+++ Project_3/src/Graph.java
@@ -2,82 +2,120 @@ import java.util.ArrayList;
 
 public class Graph {
 
-  private ArrayList<Node> nodes;
-  private ArrayList<Edge> edges;
-  
-  /**
-   * Constructs graph.
-   */
-  public Graph(){
-    nodes = new ArrayList<Node>();
-    edges = new ArrayList<Edge>();
-  }
-  
-  public boolean isEmpty() {
-    
-    return nodes.isEmpty() && edges.isEmpty();
-  
-  }
-  
-  //adds given edge.
-  public void add(Edge edge){
-    edges.add(edge);
-    nodes.add(edge.getFirstNode());
-    nodes.add(edge.getSecondNode());
-  }
-  
-  //returns true if the graph contains this edge
-  public boolean edgeExists(Edge edge) {
-    
-    for(int i = 0; i < edges.size(); i++){
-      if(edges.get(i).equals(edge)){
-        return true;
-      }
-    }
-    return false;
-  }
-  
-  //Returns true if the graph has this node
-  public boolean nodeExists(Node node) {
-    
-    for(int i = 0; i < nodes.size(); i++){
-      if(nodes.get(i).equals(node)){
-        return true;
-      }
-    }
-    return false;
-  }
-  
-  /**
-   * Returns the node with the given ID.
-   *
-   * @return node with given ID or null if absent.
-   */
-  public Node getNode(int nodeID){
-    for(int i = 0; i < nodes.size(); i++){
-      if(nodes.get(i).getid() == nodeID){
-        return nodes.get(i);
-      }
-    }
-    return null;
-  }
-  
-  //returns the array of nodes. Used in printing.
-  public ArrayList<Node> getNodeArray(){
-    return nodes;
-  }
-  
-  public Edge getRemoveEdge(){
-    Edge removed = edges.remove(0);
-    nodes.remove(removed.getFirstNode());
-    nodes.remove(removed.getSecondNode());
-    return removed;
-  }
-  
-  public Node getRemoveHigestNode() {
-    
-    Node max;
-    return null;
-  }
+	private ArrayList<Node> nodes;
+	private ArrayList<Edge> edges;
+
+	/**
+	 * Constructs graph.
+	 */
+	public Graph() {
+		nodes = new ArrayList<Node>();
+		edges = new ArrayList<Edge>();
+	}
+
+	/**
+	 * True if the graph is empty.
+	 * 
+	 * @return true if the graph is empty.
+	 */
+	public boolean isEmpty() {
+
+		return nodes.isEmpty() && edges.isEmpty();
+
+	}
+
+	/**
+	 * Adds given edge to graph.
+	 * 
+	 * @param edge
+	 *            to add to graph
+	 */
+	public void add(Edge edge) {
+		edges.add(edge);
+		nodes.add(edge.getFirstNode());
+		nodes.add(edge.getSecondNode());
+	}
+
+	/**
+	 * Returns true if the given edge is in the graph. 
+	 * Runs in O(m) time.
+	 * 
+	 * @param edge
+	 *            to search for.
+	 * @return true if edge is in graph, false otherwise.
+	 */
+	public boolean edgeExists(Edge edge) {
+
+		for (int i = 0; i < edges.size(); i++) {
+			if (edges.get(i).equals(edge)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Returns true if the graph has this node. 
+	 * Runs in O(n) time.
+	 * 
+	 * @param node
+	 *            to search for
+	 * @return true if node is in graph.
+	 */
+	public boolean nodeExists(Node node) {
+
+		for (int i = 0; i < nodes.size(); i++) {
+			if (nodes.get(i).equals(node)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Returns the node with the given ID.
+	 *
+	 * @return node with given ID or null if absent.
+	 */
+	public Node getNode(int nodeID) {
+		for (int i = 0; i < nodes.size(); i++) {
+			if (nodes.get(i).getid() == nodeID) {
+				return nodes.get(i);
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * returns the array of nodes. Used in printing.
+	 * 
+	 * @return arrayList of nodes.
+	 */
+	public ArrayList<Node> getNodeArray() {
+		return nodes;
+	}
+
+	/**
+	 * Removes an edge and returns it.
+	 * 
+	 * @return the removed edge
+	 */
+	public Edge removeEdge() {
+		Edge removed = edges.remove(0);
+		nodes.remove(removed.getFirstNode());
+		nodes.remove(removed.getSecondNode());
+		return removed;
+	}
+
+	/**
+	 * Removes/returns highest degree node.
+	 * 
+	 * @return highest degree node on the graph, which has just been removed.
+	 */
+	public Node removeHigestNode() {
+
+		Node max;
+		return null;
+	}
 
 }
--- Project_3/src/Node.java
+++ Project_3/src/Node.java
@@ -7,89 +7,99 @@ import java.util.ArrayList;
  */
 public class Node {
 
-  /**Array containing all the edges of the node*/
-  private ArrayList<Edge> edges;
-  /**ID of the node, an integer*/
-  private int id;
-  
-  /**
-   * Contructs the node with the given ID.
-   *
-   * @param id of node to be created.
-   */
-  public Node(int id){
-    this.id = id;
-  }
-  
-  /**
-   * Constructs the node with given ID.
-   * Links it to given second node.
-   *
-   * @param id of node to be created.
-   * @param node2 node that this will be linked to.
-   */  
-  public Node(int id, Node node2) {
-    this(id);
-    edges.add(new Edge(this, node2));
-  }
-  
-  /**
-   * Returns ID of this node.
-   *
-   * @return id of this node.
-   */
-  public int getid(){
-    return this.id;
-  }
-  
-  /**
-   * Returns number of edges associated with this node.
-   *
-   * @return number of edges attached to this node.
-   */
-  public int getDegree() {
-    return edges.size();
-  }
-  
-  /**
-   * Indicates if given node is attached to this node.
-   *
-   * @param node the node to check against.
-   * @return true if the nodes have an edge.
-   */
-  public boolean isAttached(Node node){
-    for(int i = 0; i < edges.size(); i++) {
-      if(edges.get(i).contains(node)){
-        return true;
-      }
-    }
-    return false;
-  }
-  
-  /**
-   * Determines if two nodes are the same.
-   * 
-   * @return true if the nodes have the same id.
-   */
-  public boolean equals (Object o){
-    if(!(o instanceof Node))
-      return false;
-    Node node = (Node) o;
-    
-    return (node.getid() == this.getid());
-  }
-  
-  /**
-   * Adds an edge to the given node.
-   *
-   * @param node node to link to.
-   */
-  public void addLink(Node node){
-    edges.add(new Edge(this, node));
-  }
-  
-  public String toString(){
-    return "" + this.id;
-  }
+	/** Array containing all the edges of the node */
+	private ArrayList<Edge> edges;
+	/** ID of the node, an integer */
+	private int id;
+
+	/**
+	 * Contructs the node with the given ID.
+	 *
+	 * @param id
+	 *            of node to be created.
+	 */
+	public Node(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * Constructs the node with given ID. Links it to given second node.
+	 *
+	 * @param id
+	 *            of node to be created.
+	 * @param node2
+	 *            node that this will be linked to.
+	 */
+	public Node(int id, Node node2) {
+		this(id);
+		edges.add(new Edge(this, node2));
+	}
+
+	/**
+	 * Returns ID of this node.
+	 *
+	 * @return id of this node.
+	 */
+	public int getid() {
+		return this.id;
+	}
+
+	/**
+	 * Returns number of edges associated with this node.
+	 *
+	 * @return number of edges attached to this node.
+	 */
+	public int getDegree() {
+		return edges.size();
+	}
+
+	/**
+	 * Indicates if given node is attached to this node.
+	 *
+	 * @param node
+	 *            the node to check against.
+	 * @return true if the nodes have an edge.
+	 */
+	public boolean isAttached(Node node) {
+		for (int i = 0; i < edges.size(); i++) {
+			if (edges.get(i).contains(node)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Determines if two nodes are the same.
+	 * 
+	 * @return true if the nodes have the same id.
+	 */
+	public boolean equals(Object o) {
+		if (!(o instanceof Node))
+			return false;
+		Node node = (Node) o;
+
+		return (node.getid() == this.getid());
+	}
+
+	/**
+	 * Adds an edge to the given node.
+	 *
+	 * @param node
+	 *            node to link to.
+	 */
+	public void addLink(Node node) {
+		edges.add(new Edge(this, node));
+	}
+
+	
+	/**
+	 * Basic override of default toString method.
+	 * 
+	 * @return string version of this node
+	 */
+	public String toString() {
+		return "" + this.id;
+	}
 
 }
