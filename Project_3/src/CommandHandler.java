24c6fb239d3bbc5f0e366c804e7db7c0674f6151
Thomas Ortz <tdortiz@ncsu.edu>
2015-11-17 18:58:47 -0500

    CommandHandler

---
+++ Project_3/src/CommandHandler.java
@@ -0,0 +1,20 @@
+import java.util.TreeMap;
+
+/**
+ * Wrapper class that handles the command input.
+ * 
+ * @author Thomas Ortiz
+ * @author Michael Mackrell
+ * @author Jacob Stone
+ * @author Curtis Moore
+ */
+public class CommandHandler {
+
+	/**
+	 * Constructor for the command handler class.
+	 * @param tree the tree use as the data structure.
+	 */
+	public CommandHandler() {
+	}
+
+}
