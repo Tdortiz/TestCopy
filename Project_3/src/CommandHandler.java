872afe7da4d0374a8b699355042a95cf19a13fa3
Thomas Ortiz <tdortiz@ncsu.edu>
2015-11-17 23:49:26 -0500

    Command Handler for input. Still need to implement this code.

--- Project_3/src/CommandHandler.java
+++ Project_3/src/CommandHandler.java
@@ -1,5 +1,3 @@
-import java.util.TreeMap;
-
 /**
  * Wrapper class that handles the command input.
  * 
@@ -16,5 +14,24 @@ public class CommandHandler {
 	 */
 	public CommandHandler() {
 	}
-
+	
+	public void isFriend(String name1, String name2){
+		
+	}
+	
+	public void mutual(String name1, String name2){
+		
+	}
+	
+	public void relation(String name1, String name2){
+		
+	}
+	
+	public void notConnected(){
+		
+	}
+	
+	public void popular(){
+		
+	}
 }
