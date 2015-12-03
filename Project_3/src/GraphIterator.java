4d515bc5fbaafb31857ce2a9d501e3ab7b8a1254
Thomas Ortz <tdortiz@ncsu.edu>
2015-12-03 15:02:21 -0500

    Javadoc GraphIterator.java

--- Project_3/src/GraphIterator.java
+++ Project_3/src/GraphIterator.java
@@ -1,10 +1,30 @@
-
+/**
+ * Iterator interface for GenericList.java
+ * 
+ * @param <E> element 
+ * 
+ * @author Thomas Ortiz
+ * @author Michael Mackrell
+ * @author Jacob Stone
+ * @author Curtis Moore
+ */
 public interface GraphIterator<E> {
 	
+	/**
+	 * Checks if the list has another element E 
+	 * @return true if there is another element false otherwise
+	 */
 	public boolean hasNext();
 	
+	/**
+	 * Returns the next element E in the list
+	 * @return the next element E
+	 */
 	public E next();
 	
+	/**
+	 * Removes an element E from the list
+	 */
 	public void remove();
 	
 	
